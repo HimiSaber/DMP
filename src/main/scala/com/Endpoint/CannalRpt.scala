c860d2864f01b8e15507e8ff75f9f87d68fd13da
HimiSaber <1399645924@qq.com>
2019-08-22 12:49:42 +0800

    更改为使用连接池来获取mysql连接的方式

--- src/main/scala/com/Endpoint/CannalRpt.scala
+++ src/main/scala/com/Endpoint/CannalRpt.scala
@@ -1,9 +1,10 @@
 package com.Endpoint
 
+import java.sql.{Connection, PreparedStatement}
 import java.util.Properties
 
 import com.typesafe.config.{Config, ConfigFactory}
-import com.utils.RptUtils
+import com.utils.{JdbcUtils, RptUtils}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
@@ -61,10 +62,32 @@ object CannalRpt {
     val ret: RDD[(String, List[Double])] = rddWithAppName.reduceByKey((a, b) => a.zip(b).map(tup => tup._1 + tup._2))
     val res: RDD[(String, Double, Double, Double, Double, Double, Double, Double, Double, Double)] = ret.map(a => (a._1, a._2(0), a._2(1), a._2(2), a._2(5), a._2(6), a._2(3), a._2(4), a._2(7),a._2(8)))
 
+    res.foreachPartition(iter=>{
+      //从连接池获取jdbc连接
+      val conn: Connection = JdbcUtils.getConnection
+      //遍历插入
+      iter.foreach(tup=>{
+        val ps: PreparedStatement = conn.prepareStatement("insert into cannalrpt1 values(?,?,?,?,?,?,?,?,?,?)")
+        ps.setString(1,tup._1)
+        ps.setDouble(2,tup._2)
+        ps.setDouble(3,tup._3)
+        ps.setDouble(4,tup._4)
+        ps.setDouble(5,tup._5)
+        ps.setDouble(6,tup._6)
+        ps.setDouble(7,tup._7)
+        ps.setDouble(8,tup._8)
+        ps.setDouble(9,tup._9)
+        ps.setDouble(10,tup._10)
+        ps.execute()
+      })
+      //回收jdbc连接
+      conn.close()
+    })
 
-    res.toDF("渠道","总请求","有效请求","广告请求数","参与竞价数","竞价成功数","展示数","点击数","DSP广告消费","DSP广告成本")
-      .write.mode(SaveMode.Append)
-        .jdbc(getProperties()._2, "CannalRpt", getProperties()._1)
+
+//    res.toDF("渠道","总请求","有效请求","广告请求数","参与竞价数","竞价成功数","展示数","点击数","DSP广告消费","DSP广告成本")
+//      .write.mode(SaveMode.Append)
+//        .jdbc(getProperties()._2, "CannalRpt", getProperties()._1)
     //res.take(20).foreach(println)
 
 
@@ -77,5 +100,8 @@ object CannalRpt {
     prop.setProperty("password",load.getString("jdbc.password"))
     prop.setProperty("Driver",load.getString("jdbc.driver"))
     (prop, load.getString("jdbc.url"))
+  }
+  def getConn(): Unit ={
+
   }
 }
